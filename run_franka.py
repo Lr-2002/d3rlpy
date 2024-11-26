a859e4cdeb6f389a034a1d84db6566506ce14f97
Lr_2002 <2629651228@qq.com>
2024-11-26 20:47:36 +0800

    feat: use wandb to run franka

---
+++ run_franka.py
@@ -0,0 +1,35 @@
+import d3rlpy
+import wandb
+from gym.wrappers  import RecordVideo
+# from d3rlpy.logger import WandbLogger
+from d3rlpy.datasets import get_minari
+dataset, env = get_minari('D4RL/kitchen/partial-v2')
+# wandb.init(project='test logger ', name='iql')
+# wandb_logger = WandbLogger()
+# prepare algorithm
+logger_adapter = d3rlpy.logging.CombineAdapterFactory([
+   d3rlpy.logging.FileAdapterFactory(root_dir="d3rlpy_logs"),
+   d3rlpy.logging.WanDBAdapterFactory(),
+])
+
+iql = d3rlpy.algos.IQLConfig(compile_graph=True).create(device="cuda:0")
+
+# train offline
+
+# train online
+# iql.fit_online(env, n_steps=1000000)
+iql.fit(
+    dataset,
+    n_steps=1000000,
+    n_steps_per_epoch=1000,
+    evaluators={
+        'environment': d3rlpy.metrics.EnvironmentEvaluator(env), # evaluate with CartPole-v1 environment
+    },
+    logger_adapter=logger_adapter
+)
+
+# # ready to control
+# env = RecordVideo(gym.make("CartPole-v1", render_mode="rgb_array"), './video')
+#
+# # evaluate
+d3rlpy.metrics.evaluate_qlearning_with_environment(iql, env)
